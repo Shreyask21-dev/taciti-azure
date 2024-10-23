eab735e6860413550b7f4087b22ba2ecb95b6d31
shreyas <shreyas.k@coinage.in>
2024-10-23 16:12:46 +0530

    'changes'

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== "production";
 export const BASEPATH = dev
-  ? "https://demo-wp-app2-aqhegvdqe2e6e9fq.southindia-01.azurewebsites.net/"
-  : "https://demo-wp-app2-aqhegvdqe2e6e9fq.southindia-01.azurewebsites.net/";
+  ? "https://taciti-wp-backend-prd-h3c8hrcfh6hme2fb.southindia-01.azurewebsites.net/"
+  : "https://taciti-wp-backend-prd-h3c8hrcfh6hme2fb.southindia-01.azurewebsites.net/";
